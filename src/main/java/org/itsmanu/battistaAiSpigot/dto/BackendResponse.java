881d2b2438b189b8d3f74e799761209341e714cb
Manuel <manuelmaterazzo@gmail.com>
2025-12-24 21:47:24 +0100

    Add BackendResponse DTO with message and flag fields.

---
+++ src/main/java/org/itsmanu/battistaAiSpigot/dto/BackendResponse.java
@@ -0,0 +1,35 @@
+package org.itsmanu.battistaAiSpigot.dto;
+
+public class BackendResponse {
+    private String message = "";
+    private Boolean flag = null;
+
+    public BackendResponse(Boolean flag, String message) {
+        this.flag = flag;
+        this.message = message;
+    }
+
+    public BackendResponse(String message) {
+        this.message = message;
+    }
+
+    public BackendResponse(boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+}
